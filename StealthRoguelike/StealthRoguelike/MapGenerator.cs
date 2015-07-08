d62759a3b8aef2068a713dbd1f8a4837fa5f6153
sidav <sidav94@gmail.com>
2015-07-08 13:00:03 +0300

    2

---
+++ StealthRoguelike/StealthRoguelike/MapGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealthRoguelike
+{
+    class MapGenerator
+    {
+        public static char[,] generateDungeon(int mapWidth, int mapHeight)
+        {
+            char[,] map = new char[mapWidth, mapHeight];
+            for (int i = 0; i < mapWidth; i++)
+                for (int j = 0; j < mapHeight; j++)
+                    map[i, j] = '#';
+            return map;
+        }
+    }
+}
--- StealthRoguelike/StealthRoguelike/Program.cs
+++ StealthRoguelike/StealthRoguelike/Program.cs
@@ -10,7 +10,12 @@ namespace StealthRoguelike
     {
         static void Main(string[] args)
         {
-
+            Console.SetBufferSize(80, 25);
+            char[,] map = MapGenerator.generateDungeon(80, 25);
+            for (int j = 0; j < 25; j++)
+                for (int i = 0; i < 80; i++)
+                    Console.Write(map[i, j]);
+            Console.ReadKey();
         }
     }
 }
