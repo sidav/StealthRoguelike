db847b9e11866c00ea5c68d147c1a5e2f5edf049
sidav <sidav94@gmail.com>
2015-07-08 22:42:55 +0300

    rendering

--- StealthRoguelike/StealthRoguelike/Program.cs
+++ StealthRoguelike/StealthRoguelike/Program.cs
@@ -8,6 +8,21 @@ namespace StealthRoguelike
 {
     class Program
     {
+        static void drawTestMap(char[,] map)
+        {
+            for (int j = 0; j < 25; j++)
+                for (int i = 0; i < 80; i++)
+                    //if (i != 79 || j != 24)
+                    if (i > 0 && i < 80 - 1 && j > 0 && j < 25 - 1)
+                        if (map[i - 1, j - 1] != '#' || map[i, j - 1] != '#'
+                            || map[i + 1, j - 1] != '#' || map[i - 1, j] != '#' ||
+                            map[i + 1, j] != '#' || map[i - 1, j + 1] != '#'
+                            || map[i, j + 1] != '#' || map[i + 1, j + 1] != '#')
+                            Console.Write(map[i, j]);
+                        else Console.Write(' ');
+                    else Console.Write(map[i, j]);
+        }
+
         static void Main(string[] args)
         {
             Console.SetBufferSize(80, 26);
@@ -15,10 +30,7 @@ namespace StealthRoguelike
             Console.CursorVisible = false;
             MapGenerator.setParams(80, 25);
             char[,] map = MapGenerator.generateDungeon();
-            for (int j = 0; j < 25; j++)
-                for (int i = 0; i < 80; i++)
-                    //if (i != 79 || j != 24)
-                     Console.Write(map[i, j]);
+            drawTestMap(map);
             Console.ReadKey();
         }
     }
