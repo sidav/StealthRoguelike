46d0c4e23144647f7f6e0bf4f83750a51d0e02c5
sidav <sidav94@gmail.com>
2015-08-10 18:41:32 +0300

    Line handling implemented a quite more

--- StealthRoguelike/StealthRoguelike/Line.cs
+++ StealthRoguelike/StealthRoguelike/Line.cs
@@ -10,19 +10,58 @@ namespace StealthRoguelike
     {
         public static int CurX = 0, CurY = 0;
         static bool lineEnded = true;
-        static int fromX, fromY, toX, toY;
-        static void Init(int startX, int startY, int endX, int endY)
+        static int fromX, fromY, toX, toY, deltax, deltay, err, deltaerr, 
+            xmod, ymod;
+
+        public static void Init(int startX, int startY, int endX, int endY)
         {
             fromX = startX;
             fromY = startY;
             toX = endX;
             toY = endY;
+            CurX = fromX;
+            CurY = fromY;
+            deltax = Math.Abs(toX - fromX);
+            deltay = Math.Abs(toY - fromY);
+            err = 0;
+            xmod = 1;
+            ymod = 1;
+            if (toX < fromX)
+                xmod = -1;
+            if (toY < fromY)
+                ymod = -1;
         }
-        static bool Step() //traverse to next point of the line. 
+
+        public static bool Step() //traverse to next point of the line. 
         {                  //also returns true if the end is reached
-            bool endReached = false;
+            bool endReached = false; 
 
+            if (deltax >= deltay)
+            {
+                deltaerr = deltay;
+                CurX += xmod;
+                err += deltaerr;
+                if (2 * err >= deltax)
+                {
+                    CurY += ymod;
+                    err -= deltax;
+                }
+            }
+            if (deltax < deltay)
+            {
+                deltaerr = deltax;
+                CurY += ymod;
+                err += deltaerr;
+                if (2 * err >= deltay)
+                {
+                    CurX += xmod;
+                    err -= deltay;
+                }
+            }
 
+            // check whenether we reached the end of the line
+            if (CurX == toX && CurY == toY) 
+                endReached = true;
             return endReached;
         }
 
--- StealthRoguelike/StealthRoguelike/Program.cs
+++ StealthRoguelike/StealthRoguelike/Program.cs
@@ -27,11 +27,36 @@ namespace StealthRoguelike
                     else Console.Write('#');
         }
 
+        static void drawLine(int fx, int fy, int tx, int ty)
+        {
+            Line.Init(fx, fy, tx, ty);
+            do
+            {
+                if (Line.CurX < consoleWidth && Line.CurY < consoleHeight && Line.CurX >=0 && Line.CurY >= 0)
+                {
+                    Console.SetCursorPosition(Line.CurX, Line.CurY);
+                    Console.Write('#');
+                }
+            }
+            while (!Line.Step());
+        }
+
         static void Main(string[] args)
         {
             Console.SetWindowSize(consoleWidth, consoleHeight);
             Console.SetBufferSize(consoleWidth, consoleHeight);
             Console.CursorVisible = false;
+
+            //LINE DRAWING TEST
+            //for (int i = 0; i < consoleWidth; i++)
+            //    for (int j = 0; j < consoleHeight; j++)
+            //        if ((40-i)*(40-i) + (12-j)*(12-j) <= 25)
+            //            drawLine(40, 12, i, j);
+            drawLine(5, 20, 10, 5);
+            drawLine(50, 10, 40, 12);
+            Console.ReadKey(true);
+            //END OF TEST
+
             MapGenerator.setParams(mapWidth, mapHeight);
             //char[,] map = MapGenerator.generateDungeon();
             //drawTestMap(map);
