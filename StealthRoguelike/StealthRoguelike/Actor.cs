5a749bf2c3b001dd12d7e292615cde3e1483f1a4
Не Скажу <sidav94@gmail.com>
2015-10-03 20:57:16 +0300

    Modified the AI to allow notice the player. BARELY WORKS, NEEDS TO REWORK

--- StealthRoguelike/StealthRoguelike/Actor.cs
+++ StealthRoguelike/StealthRoguelike/Actor.cs
@@ -9,16 +9,38 @@ namespace StealthRoguelike
     class Actor:Unit //it's just like an unit, but with AI
     {
 
-        public enum State {waiting,patrolling} //to be expanded...
+        public enum State {waiting,patrolling, alerted} //to be expanded...
 
         public State currentState;
+
 
         public Actor(string name,int x, int y, char appear):base(name, x,y,appear,true,ConsoleColor.Red)
         {
             currentState = State.patrolling;
         }
 
-
+        public void Draw()
+        {
+            if (currentState != State.alerted)
+                base.Draw();
+            else
+            {
+                ConsoleColor tempFore = Console.ForegroundColor;
+                ConsoleColor tempBack = Console.BackgroundColor;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.BackgroundColor = color;
+                Console.SetCursorPosition(this.coordX, this.coordY);
+                Console.Write('!');
+                Console.ForegroundColor = color;
+                Console.BackgroundColor = tempBack;
+                if (hasFOV) //draw a thingy that shows this unit's direction
+                {
+                    drawLookingThingy();
+                }
+                Console.ForegroundColor = tempFore;
+            }
+            
+        }
 
         ///////
         //AI!//
@@ -34,30 +56,56 @@ namespace StealthRoguelike
             } while ((lookX == 0 && lookY == 0) || !World.IsPassable(coordX + lookX, coordY + lookY));
         }
 
+        void Check() //Well, does this actor see anything interesting?
+        {
+            int targetX, targetY;
+            //do we see the player?
+            targetX = World.player.coordX;
+            targetY = World.player.coordY;
+            int vectorX = World.player.coordX - coordX;
+            int vectorY = World.player.coordY - coordY;
+            double distance = Math.Sqrt(vectorX * vectorX + vectorY * vectorY);
+            if (distance <= visibilityRadius)
+            {
+                if (ViewSector.PointIsInSector(coordX, coordY, targetX, targetY, lookX, lookY, ViewingAngle))
+                    if (World.VisibleLineExist(coordX, coordY, targetX, targetY))
+                    {
+                        //MORE CODE EXPECTING
+                        Log.AddLine(Name + " notices you!");
+                        currentState = State.alerted;
+                        return;
+                    }
+            }
+        }
+
         public void DoSomething() //AI itself
         {
+            Check();
             //if is waiting for something then do nothing, huh
             if (currentState == State.waiting)
                 return; //BUT IT'S NOT WHAT WE NEED TO!
-            //close door if neccessary
-            if (World.TryCloseDoor(coordX - lookX, coordY - lookY))
-                return;
-            //let's SUDDENLY turn to the random direction, maybe? :D
-            if (Algorithms.getRandomInt(suddenTurningFrequency) == 0)
+            if (currentState == State.patrolling)
             {
-                turnToPassable();
-                return;
-            }
-            //Move forward if there is nothing to do...
-            if (World.IsPassable(coordX + lookX, coordY + lookY))
-            {
-                MoveForward();
-                return;
-            }
-            else //or open door if there is. Otherwise turn to random direction
-            {
-                if (!World.TryOpenDoor(coordX + lookX, coordY + lookY))
+                //close door if neccessary
+                if (World.TryCloseDoor(coordX - lookX, coordY - lookY))
+                    return;
+                //let's SUDDENLY turn to the random direction, maybe? :D
+                if (Algorithms.getRandomInt(suddenTurningFrequency) == 0)
+                {
                     turnToPassable();
+                    return;
+                }
+                //Move forward if there is nothing to do...
+                if (World.IsPassable(coordX + lookX, coordY + lookY))
+                {
+                    MoveForward();
+                    return;
+                }
+                else //or open door if there is. Otherwise turn to random direction
+                {
+                    if (!World.TryOpenDoor(coordX + lookX, coordY + lookY))
+                        turnToPassable();
+                }
             }
 
         }
--- StealthRoguelike/StealthRoguelike/Unit.cs
+++ StealthRoguelike/StealthRoguelike/Unit.cs
@@ -11,6 +11,7 @@ namespace StealthRoguelike
         public string Name;
         public int coordX, coordY; //global coords
         public int lookX = 0, lookY = -1;   //heading vector
+        public int ViewingAngle = 90; //in degrees.
         public int visibilityRadius = 5; //basic visibility distance
         public char appearance;
         public bool hasFOV;
@@ -42,22 +43,25 @@ namespace StealthRoguelike
             Console.ForegroundColor = color;
             Console.SetCursorPosition(this.coordX, this.coordY);
             Console.Write(this.appearance);
-            if (hasFOV) //draw a thingy that shows this unit's direction
-            {
-                char thingy = '?';
-                if (lookX == 0)
-                    thingy = '|';
-                if (lookY == 0)
-                    thingy = '-';
-                if (lookX * lookY == 1)
-                    thingy = '\\';
-                if (lookX * lookY == -1)
-                    thingy = '/';
-                Console.SetCursorPosition(coordX + lookX, coordY + lookY);
-                Console.Write(thingy);
-                //TODO!
-            }
+            if (hasFOV)
+                drawLookingThingy();
             Console.ForegroundColor = temp;
+        }
+
+        protected void drawLookingThingy() //draw a thingy that shows this unit's direction
+        {
+            char thingy = '?';
+            if (lookX == 0)
+                thingy = '|';
+            if (lookY == 0)
+                thingy = '-';
+            if (lookX * lookY == 1)
+                thingy = '\\';
+            if (lookX * lookY == -1)
+                thingy = '/';
+            Console.SetCursorPosition(coordX + lookX, coordY + lookY);
+            Console.Write(thingy);
+            //TODO!
         }
 
         public void MoveForward() //move where this unit looks
