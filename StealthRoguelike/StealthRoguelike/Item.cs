f0ee39a63382f7b558367f793a795d72180ee901
sidav <sidav94@gmail.com>
2016-01-27 12:12:29 +0300

    Working on items

---
+++ StealthRoguelike/StealthRoguelike/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealthRoguelike
+{
+    class Item //Any thing that can be picked up.
+    {
+        int weight = 1;
+
+    }
+}
