e55ac2c8c4ae0778c295ef283800e455d45f0cb6
sidav <sidav94@gmail.com>
2016-05-02 20:16:58 +0300

    TurnTiming was missing

---
+++ StealthRoguelike/StealthRoguelike/TurnTiming.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StealthRoguelike
+{
+    class TurnTiming
+    {
+        public static int CurrentTurn = 0;
+
+        int NextActionTurn = 0;
+
+        public void AddActionTime(int time)
+        {
+            NextActionTurn += time;
+        }
+
+        public int GetCurrentTurn()
+        {
+            return CurrentTurn;
+        }
+
+        public bool IsTimeToAct()
+        {
+            if (NextActionTurn < CurrentTurn)
+                NextActionTurn = CurrentTurn;
+            if (NextActionTurn == CurrentTurn)
+                return true;
+            return false;
+        }
+
+        public static void Tick()
+        {
+            CurrentTurn++;
+        }
+    }
+}
